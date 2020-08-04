0b471a90b38b8a58dd60134b9172ff4a274ebf4c
JapanPanda <japanxpanda@yahoo.com>
2020-08-04 00:41:28 -0700

    Added backwards functionality

--- tastinder-frontend/pages/host.js
+++ tastinder-frontend/pages/host.js
@@ -3,8 +3,21 @@ import NavBar from '../components/navbar';
 import styles from '../styles/host.module.scss';
 import React, { useState } from 'react';
 
+// TODO: Implement Autocomplete in location
+
 const Host = () => {
   const [card, setCard] = useState(0);
+  const [inCard, setInCard] = useState(-1);
+  const [outCard, setOutCard] = useState([false, false, false]);
+
+  const [location, setLocation] = useState('');
+  const [keyword, setKeyword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  const handleInput = (event, set) => {
+    set(event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -12,12 +25,22 @@ const Host = () => {
   };
 
   const nextButtonClick = (event) => {
-    setCard(card + 1);
-    console.log('Card: ' + card);
+    let currCard = card;
+    setCard(currCard + 1);
+    let currOutCard = outCard;
+    currOutCard[currCard] = true;
+    setOutCard(currOutCard);
+
+    console.log('Card: ' + currCard);
   };
 
   const backButtonClick = (event) => {
-    setCard(card - 1);
+    let currCard = card;
+    setCard(currCard - 1);
+    setInCard(currCard - 1);
+
+    let currOutCard = outCard;
+    currOutCard[currCard - 1] = false;
     console.log('Card: ' + card);
   };
 
@@ -29,33 +52,105 @@ const Host = () => {
       </Head>
 
       <NavBar />
+
       <div className={styles.formContainer}>
         <div
+          style={{ zIndex: 4 }}
           className={
-            `${styles.initialCard} ${styles.red}` +
-            (card !== 0 ? ` ${styles.dissapear}` : ` ${styles.show}`)
+            `${styles.card} ${styles.red} ` +
+            (inCard === 0 ? `${styles.slideIn} ` : ``) +
+            (outCard[0] === true ? `${styles.slideOut} ` : ``)
           }
         >
           <div className={styles.cardBody}>
             <h1>Location</h1>
             <div className={styles.cardField}>
-              <input type="text" placeholder="City, Address, etc..." />
+              <input
+                type="text"
+                placeholder="City, Address, etc..."
+                onInput={(event) => handleInput(event, setLocation)}
+              />
             </div>
-            <button onClick={nextButtonClick}>Next→</button>
+            <button className={styles.right} onClick={nextButtonClick}>
+              Next→
+            </button>
           </div>
         </div>
 
-        {card === 1 && (
-          <div className={`${styles.card} ${styles.green}`}>
-            <div className={styles.cardBody}>
-              <h1>Keyword</h1>
-              <div className={styles.cardField}>
-                <input type="text" placeholder="Sushi, burgers, etc..." />
-              </div>
-              <button onClick={nextButtonClick}>Next→</button>
+        <div
+          style={{ zIndex: 3 }}
+          className={
+            `${styles.card} ${styles.green} ` +
+            (inCard === 1 ? `${styles.slideIn} ` : ``) +
+            (outCard[1] === true ? `${styles.slideOut} ` : ``)
+          }
+        >
+          <div className={styles.cardBody}>
+            <h1>Keyword</h1>
+            <div className={styles.cardField}>
+              <input
+                type="text"
+                placeholder="(Optional) Sushi, burgers, etc..."
+                onInput={(event) => handleInput(event, setKeyword)}
+              />
             </div>
+            <button onClick={backButtonClick}>←Back</button>
+            <button className={styles.right} onClick={nextButtonClick}>
+              Next→
+            </button>
           </div>
-        )}
+        </div>
+
+        <div
+          style={{ zIndex: 2 }}
+          className={
+            `${styles.card} ${styles.red} ` +
+            (inCard === 2 ? `${styles.slideIn} ` : ``) +
+            (outCard[2] === true ? `${styles.slideOut} ` : ``)
+          }
+        >
+          <div className={styles.cardBody}>
+            <h1>Create Room?</h1>
+            <div className={styles.cardField}>
+              <p>Ready to find your true love?</p>
+            </div>
+
+            <button onClick={backButtonClick}>←Back</button>
+            <button
+              className={`${styles.right} ${styles.createButton}`}
+              onClick={nextButtonClick}
+            >
+              Create→
+            </button>
+          </div>
+        </div>
+
+        {/* This is where the room gets generated and the room code is generated */}
+        <div
+          style={{ zIndex: 1 }}
+          className={
+            `${styles.card} ${styles.red} ` +
+            (inCard === 3 ? `${styles.slideIn} ` : ``) +
+            (outCard[3] === true ? `${styles.slideOut} ` : ``)
+          }
+        >
+          {true && <div className={styles.loading}>Loading</div>}
+
+          {/* <div className={styles.cardBody}>
+            <h1>Create Room?</h1>
+            <div className={styles.cardField}>
+              <p>Ready to find your true love?</p>
+            </div>
+
+            <button onClick={backButtonClick}>←Back</button>
+            <button
+              className={`${styles.right} ${styles.createButton}`}
+              onClick={nextButtonClick}
+            >
+              Create→
+            </button> */}
+          {/* </div> */}
+        </div>
       </div>
     </div>
   );
